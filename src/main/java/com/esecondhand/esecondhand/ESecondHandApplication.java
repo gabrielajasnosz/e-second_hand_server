b743437640bf5f82040e4ce9698b8d12f4c3c0b2
gabrielajasnosz <gabriela.jasnosz@gmail.com>
2021-09-18 15:29:10 +0200

    init

---
+++ src/main/java/com/esecondhand/esecondhand/ESecondHandApplication.java
@@ -0,0 +1,13 @@
+package com.esecondhand.esecondhand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ESecondHandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ESecondHandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/esecondhand/esecondhand/ESecondHandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esecondhand.esecondhand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ESecondHandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
